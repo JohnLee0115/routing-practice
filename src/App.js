860d1e4ebe45643cd5ed64a9b3b9857008c3b4d2
johnlee0115 <johnlee011500@gmail.com>
2023-07-13 19:07:19 -0700

    routing_practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useParams } from 'react-router';
 import './App.css';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
+
+const Home = (props) => {
+  return (
+    <h1>Welcome</h1>
+  );
+}
+
+const Input = (props) => {
+  const { value } = useParams();
+  const { color1 } = useParams("");
+  const { color2 } = useParams("");
+
+
+  return (
+    isNaN(value) ?
+    <div style={{backgroundColor: color2}}>
+      <h1 style={{color: color1 }}>The word is: { value } </h1>
+    </div>:
+    <div style={{backgroundColor: color2}}>
+      <h1 style={{color: color1 }}>The number is: { value } </h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to={"/home"}>Go Home</Link>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/:value' element={<Input />} />
+        <Route path='/:value/:color1' element={<Input />} />
+        <Route path='/:value/:color1/:color2' element={<Input />} />
+      </Routes>
     </div>
   );
 }
